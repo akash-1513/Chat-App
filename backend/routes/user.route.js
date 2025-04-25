8dc79adda13466d32627e9a9525bc650422f3fa3
Akash Kumar <akashkumar878944@gmail.com>
2025-04-26 04:43:55 +0530

    Setup route for login and register

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,10 @@
 import {Router} from "express"
+import { loginUser, registerUser } from "../controllers/user.controller.js"
 
 const router = Router()
 
 // routes
-
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
 export default router
